3e6db6d609cfc2c4aed30940f4a7c75b31b5bf1b
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-01-17 13:10:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppSearch from './components/AppSearch.vue';
+import AppFilms from './components/AppFilms.vue';
+import {store} from './store.js';
+
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFilms,
+    AppSearch,
+  },
+  data() {
+    return {
+      store
+    }
+  },
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss' as *;
+
+</style>
---
+++ src/components/AppFilms.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+   
+   
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+    
+   
+</style>
